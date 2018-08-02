97a845f91026458278d2721ada40ced58fcfdd51
ananliangliang <ananliangliang@vip.qq.com>
2018-08-02 20:23:58 +0800

    峰峰的里程碑

---
+++ huge-sso/src/main/java/com/huge/web/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.huge.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+	@GetMapping("user/login")
+	public String goLogin() {
+		return
+	}
+}
